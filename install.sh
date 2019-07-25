0ea894b3c05b61977d0a42fb6722f4187140fa7d
Liu <shihaol@38f9d36a291f.ant.amazon.com>
2019-07-25 16:13:58 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ sudo rm /usr/lib/libplist*
 sudo rm /usr/bin/iproxy 
 sudo rm /usr/lib/libusbmuxd.*
 
-# libimobiledevice
+# libimobiledevice & ideviceinstaller
 sudo rm /usr/bin/idevice*
 sudo rm /usr/lib/libimobiledevice.*
 
@@ -18,13 +18,18 @@ sudo rm /usr/lib/libimobiledevice.*
 sudo rm /usr/bin/openssl
 sudo rm /usr/lib/libssl*
 
+# usbmuxd
+sudo rm /usr/sbin/usbmuxd
+
 # libcrypto
 sudo rm /usr/lib/libcrypto*
 
-# copy all bin & lib
+# copy all bin & sbin & lib
 sudo chmod 755 fakeroot/usr/bin/*
+sudo chmod 755 fakeroot/usr/sbin/*
 sudo chmod 755 fakeroot/usr/lib/*
 sudo cp -p fakeroot/usr/bin/* /usr/bin
+sudo cp -p fakeroot/usr/sbin/* /usr/sbin
 sudo cp -p fakeroot/usr/lib/* /usr/lib
 
 # test
