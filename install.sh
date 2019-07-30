5c5bfcda9bf81189d2cd5a3e039a073e30a23343
Liu <shihaol@38f9d36a291f.ant.amazon.com>
2019-07-30 10:57:27 -0700

    update install.sh

--- install.sh
+++ install.sh
@@ -2,28 +2,30 @@
 
 set -x
 
+ROOT=
+
 # libplist
-sudo rm /usr/bin/plistutil
-sudo rm /usr/lib/libplist*
+sudo rm ${ROOT}/usr/bin/plistutil
+sudo rm ${ROOT}/usr/lib/libplist*
 
 # libusbmuxd
-sudo rm /usr/bin/iproxy 
-sudo rm /usr/lib/libusbmuxd.*
+sudo rm ${ROOT}/usr/bin/iproxy 
+sudo rm ${ROOT}/usr/lib/libusbmuxd.*
 
 # libimobiledevice & ideviceinstaller
-sudo rm /usr/bin/idevice*
-sudo rm /usr/lib/libimobiledevice.*
+sudo rm ${ROOT}/usr/bin/idevice*
+sudo rm ${ROOT}/usr/lib/libimobiledevice.*
 
 # usbmuxd
-sudo rm /usr/sbin/usbmuxd
+sudo rm ${ROOT}/usr/sbin/usbmuxd
 
 # copy all bin & sbin & lib
 sudo chmod 755 fakeroot/usr/bin/*
 sudo chmod 755 fakeroot/usr/sbin/*
 sudo chmod 755 fakeroot/usr/lib/*
-sudo cp -p fakeroot/usr/bin/* /usr/bin
-sudo cp -p fakeroot/usr/sbin/* /usr/sbin
-sudo cp -p fakeroot/usr/lib/* /usr/lib
+sudo cp -p fakeroot/usr/bin/* ${ROOT}/usr/bin
+sudo cp -p fakeroot/usr/sbin/* ${ROOT}/usr/sbin
+sudo cp -p fakeroot/usr/lib/* ${ROOT}/usr/lib
 
 # test
 plistutil
