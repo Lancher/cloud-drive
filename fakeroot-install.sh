eea1a364da44424dccbce5fca9b7e5d0446c9b8b
SHI HAO LIU <shihaol@amazon.com>
2019-08-22 17:41:13 -0700

    update rootfs script

--- fakeroot-install.sh
+++ fakeroot-install.sh
@@ -20,12 +20,12 @@ sudo rm ${ROOT}/usr/lib/libimobiledevice.*
 sudo rm ${ROOT}/usr/sbin/usbmuxd
 
 # copy all bin & sbin & lib
-sudo chmod 755 fakeroot/usr/bin/*
-sudo chmod 755 fakeroot/usr/sbin/*
-sudo chmod 755 fakeroot/usr/lib/*
-sudo cp -p fakeroot/usr/bin/* ${ROOT}/usr/bin
-sudo cp -p fakeroot/usr/sbin/* ${ROOT}/usr/sbin
-sudo cp -p fakeroot/usr/lib/* ${ROOT}/usr/lib
+sudo chmod 755 imobiledevice-fakeroot/usr/bin/*
+sudo chmod 755 imobiledevice-fakeroot/usr/sbin/*
+sudo chmod 755 imobiledevice-fakeroot/usr/lib/*
+sudo cp -p imobiledevice-fakeroot/usr/bin/* ${ROOT}/usr/bin
+sudo cp -p imobiledevice-fakeroot/usr/sbin/* ${ROOT}/usr/sbin
+sudo cp -p imobiledevice-fakeroot/usr/lib/* ${ROOT}/usr/lib
 
 # test
 plistutil
